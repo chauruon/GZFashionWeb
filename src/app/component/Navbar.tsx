2ce5082d9113e3a5dc31e4ba123d798fee1d348e
chauruon <chauruon.lci@gmail.com>
2023-07-18 11:35:11 +0700

    test use tailwins

---
+++ src/app/component/Navbar.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <nav className='py-3 flex flex-row bg-[#f8e71c]'>
+      <h2>
+        home
+      </h2>
+      <h2>
+        home
+      </h2>
+      <h2>
+        home
+      </h2>
+    </nav>
+  )
+}
+
+export default Navbar
